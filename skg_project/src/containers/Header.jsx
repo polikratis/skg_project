9acf8165642a5c6d801e83ac90d6ff4451defc41
acheird <acheird@gmail.com>
2024-07-18 13:35:19 +0300

    header css

--- skg_project/src/containers/Header.jsx
+++ skg_project/src/containers/Header.jsx
@@ -5,14 +5,21 @@ const Header = () => {
   return (
     <div className="app__header" id="home">
       <div className="app__header-content">
-        <h1 className="gradient__text"></h1>
-        <p></p>
+        <h1 className="gradient__text">
+          Stay Informed: Subscribe to Our Newsletter!
+        </h1>
+        <p>
+          As you explore the captivating streets of Thessaloniki, don’t miss out
+          on the exciting events happening around the city! From cultural
+          festivals to gastronomic delights, there’s always something happening.
+        </p>
         <div className="app__header-content__input">
           <input type="email" placeholder="Your Email Adress" />
+          <button type="button">Get Started</button>
         </div>
         <div className="app__header-content__people">
           <img src={people} alt="people" />
-          <p>500 people requested access a visit in last 24 hours</p>
+          <p>1600 people requested access a visit in last 24 hours</p>
         </div>
       </div>
     </div>
