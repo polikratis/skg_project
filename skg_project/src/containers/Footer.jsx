61942820005d8c04d4c0c63fe9e602f6f0a9f4c0
acheird <acheird@gmail.com>
2024-07-19 12:42:49 +0300

    footer change

--- skg_project/src/containers/Footer.jsx
+++ skg_project/src/containers/Footer.jsx
@@ -1,11 +1,9 @@
 import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 import "./footer.css";
-import skg2 from "../assets/skg2.jpg";
 
 const Footer = () => {
   return (
     <div className="app__footer">
-      {/* <div className="app__footer-links_logo"> */}
       <p className="p__opensans">
         &quot;You don't need magic to disappear, all you need is a
         destination!&quot;
@@ -15,7 +13,7 @@ const Footer = () => {
         <FiTwitter />
         <FiInstagram />
       </div>
-      {/* </div> */}
+
       <div className="footer__copyright">
         <p className="p__opensans">2024 lala. All Rights reserved.</p>
       </div>
