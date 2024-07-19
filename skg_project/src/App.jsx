dd8067268e39668e229ba055be1a552d6be5b20f
acheird <acheird@gmail.com>
2024-07-19 14:38:00 +0300

    add intro container

--- skg_project/src/App.jsx
+++ skg_project/src/App.jsx
@@ -5,15 +5,17 @@ import About from "./containers/About";
 import What2do from "./containers/What2do";
 import Footer from "./containers/Footer";
 import Gallery from "./containers/Gallery";
+import Intro from "./containers/Intro";
 
 function App() {
   return (
     <>
       <Navbar />
-      <NewsLetter />
       <About />
       <What2do />
+      <Intro />
       <Gallery />
+      <NewsLetter />
       <Footer />
     </>
   );
