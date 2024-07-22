a8301461ed2cda511248f348ce621cc3230e97ba
acheird <acheird@gmail.com>
2024-07-22 16:38:30 +0300

    article css

--- skg_project/src/components/Article/Article.jsx
+++ skg_project/src/components/Article/Article.jsx
@@ -2,11 +2,11 @@ import "./article.css";
 
 const Article = ({ imgUrl, date, title }) => {
   return (
-    <div className="gpt3__blog-container_article">
-      <div className="gpt3__blog-container_article-image">
+    <div className="app__blog-container_article">
+      <div className="app__blog-container_article-image">
         <img src={imgUrl} alt="blog_image" />
       </div>
-      <div className="gpt3__blog-container_article-content">
+      <div className="app__blog-container_article-content">
         <div>
           <p>{date}</p>
           <h3>{title}</h3>
