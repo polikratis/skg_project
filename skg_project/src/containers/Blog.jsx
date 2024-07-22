384f99dee375f3eb993bb1f193b9fb23150e8268
acheird <acheird@gmail.com>
2024-07-22 16:41:23 +0300

    blog css

--- skg_project/src/containers/Blog.jsx
+++ skg_project/src/containers/Blog.jsx
@@ -1,8 +1,10 @@
 import "./blog.css";
 import { Article } from "../components";
-import thess1 from "../assets/thess1.jpg";
-
-import React from "react";
+import article1 from "../assets/festival.jpg";
+import article2 from "../assets/louloudadika.png";
+import article3 from "../assets/olympus.jpg";
+import article4 from "../assets/Best-Restaurants.jpg";
+import article5 from "../assets/roman-agora.jpg";
 
 const Blog = () => {
   return (
@@ -14,26 +16,30 @@ const Blog = () => {
       </div>
       <div className="app__blog-container">
         <div className="app__blog-container_groupA">
-          <Article imgUrl={thess1} date="" />
+          <Article
+            imgUrl={article1}
+            date="June 24, 2024"
+            title="Music, singing, opera and poetry!"
+          />
         </div>
         <div className="app__blog-container_groupB">
           <Article
-            imgUrl={thess1}
+            imgUrl={article2}
             date="June 26, 2024"
             title="48 Hours in Thessaloniki: Louloudadika and Beyond"
           />
           <Article
-            imgUrl={thess1}
+            imgUrl={article3}
             date="June 27, 2024"
             title="From Olympus to Meteora: Day Trips from Thessaloniki"
           />
           <Article
-            imgUrl={thess1}
+            imgUrl={article4}
             date="June 28, 2024"
             title="Culinary Adventures: Thessaloniki’s Food Scene Unveiled"
           />
           <Article
-            imgUrl={thess1}
+            imgUrl={article5}
             date="June 29, 2024"
             title="Exploring Ancient Treasures: A Weekend Itinerary in Thessaloniki"
           />
